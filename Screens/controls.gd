a5170afc2a37cb95be8bf2c21ea8b382e4522c5e
Ishaan Saha <40528898+ishaaneagles@users.noreply.github.com>
2023-12-13 11:56:02 -0800

    added a controls (help) screen

---
+++ Screens/controls.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://Screens/mainmenu.tscn")
+
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://World/main.tscn")
